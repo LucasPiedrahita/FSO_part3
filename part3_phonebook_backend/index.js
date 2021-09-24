f291ac4a6be787626b4d2988d5743df1e96274db
LucasPiedrahita <Lucas.Piedrahita96@gmail.com>
2021-09-23 20:31:53 -0400

    fix typo that cause npm start failure in heroku

--- part3_phonebook_backend/index.js
+++ part3_phonebook_backend/index.js
@@ -139,7 +139,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PROT || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
